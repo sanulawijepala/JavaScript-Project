05f33c134562d23846f53826985f25b9c6c031e4
MPR Student <mpr@comscieng.local>
2025-04-26 11:55:46 +0530

    bugs fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
   {
-    files: ["src/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "no-unused-vars": "error", // or "error" to treat it as an error
+    files: ['src/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser
     },
-  },
+    rules: {
+      'no-unused-vars': 'error'
+    }
+  }
 ]);
